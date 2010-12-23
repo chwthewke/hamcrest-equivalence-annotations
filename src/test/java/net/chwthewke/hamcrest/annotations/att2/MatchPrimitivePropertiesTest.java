e7161a113bfd73850e4b8dc4db2162476b39c19d
Thomas Dufour <chwthewke@gmail.com>
2010-12-23 17:53:47 +0100

    [hamcrest-annotations] * ignored orking test of the primitives issue.

--- src/test/java/net/chwthewke/hamcrest/annotations/att2/MatchPrimitivePropertiesTest.java
+++ src/test/java/net/chwthewke/hamcrest/annotations/att2/MatchPrimitivePropertiesTest.java
@@ -2,9 +2,7 @@ package net.chwthewke.hamcrest.annotations.att2;
 
 import static net.chwthewke.hamcrest.annotations.att2.CompositeMatcherFactory.matcherBySpecification;
 import net.chwthewke.hamcrest.annotations.declarations.ReflectivePrimitiveMatch;
-import net.chwthewke.hamcrest.annotations.declarations.ReflectivePrimitiveMatch.Matched;
 
-import org.hamcrest.Matcher;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -15,7 +13,7 @@ public class MatchPrimitivePropertiesTest {
     public void createMatcherOfPrimitiveProperties( ) throws Exception {
         // Setup
         // Exercise
-        final Matcher<Matched> matcher = matcherBySpecification(
+        matcherBySpecification(
             ReflectivePrimitiveMatch.Matched.class,
             ReflectivePrimitiveMatch.MatcherSpecification.class )
             .of( new ReflectivePrimitiveMatch.Matched( 1, 4 ) );
