0160a7d75c9f521758e80275e157fdbf89f56c8c
Thomas Dufour <chwthewke@gmail.com>
2011-01-25 08:50:07 +0100

    [hamcrest-equivalence-annotations] * pom: no more jarjar for now

--- src/main/java/net/chwthewke/hamcrest/annotations/ByEquivalence.java
+++ src/main/java/net/chwthewke/hamcrest/annotations/ByEquivalence.java
@@ -33,7 +33,7 @@ import net.chwthewke.hamcrest.matchers.Equivalence;
  * The {@link ByEquivalence} annotation specifies equivalence according to a pre-existing equivalence,
  * allowing to reuse existing {@link org.hamcrest.Matcher}s in an equivalence, albeit somewhat
  * indirectly, as one must provide a concrete class implementing
- * {@link net.chwthewke.hamcrest.matchers.Equivalence}.
+ * {@link net.chwthewke.hamcrest.matchers.Equivalence} with a no-argument constructor.
  */
 public @interface ByEquivalence {
     /**
