d33402a2e2b0fc11bdb1b52b041ad15fe4f3821a
Thomas Dufour <chwthewke@gmail.com>
2011-01-29 03:15:24 +0100

    [hamcrest-annotations] * Equivalence testing matcher

--- src/main/java/net/chwthewke/hamcrest/matchers/EquivalenceAnnotationInterpreter.java
+++ src/main/java/net/chwthewke/hamcrest/matchers/EquivalenceAnnotationInterpreter.java
@@ -2,8 +2,7 @@ package net.chwthewke.hamcrest.matchers;
 
 import java.lang.reflect.Method;
 
-
-public interface EquivalenceAnnotationInterpreter<T> {
+interface EquivalenceAnnotationInterpreter<T> {
 
     Equivalence<T> interpretAnnotation( Method specificationMethod, Class<T> propertyType );
 
