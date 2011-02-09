48cd60b88f4723b88da87bf9020c6bd79a4084e4
Thomas Dufour <chwthewke@gmail.com>
2011-02-09 16:49:50 +0100

    [hamcrest-equivalence-annotations] * [wip] @Text use case test

--- src/uct/java/net/chwthewke/hamcrest/annotations/TextMatchersTest.java
+++ src/uct/java/net/chwthewke/hamcrest/annotations/TextMatchersTest.java
@@ -1,9 +1,42 @@
 package net.chwthewke.hamcrest.annotations;
 
+import static net.chwthewke.hamcrest.MatcherUtils.describe;
+import static net.chwthewke.hamcrest.equivalence.TextEquivalenceOption.IGNORE_CASE;
+import static net.chwthewke.hamcrest.matchers.EquivalenceMatchers.asSpecifiedBy;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import org.hamcrest.Matcher;
 import org.junit.Ignore;
+import org.junit.Test;
 
 @Ignore
 public class TextMatchersTest {
+
+    @Test
+    public void matchWithTextEquivalenceWithCaseOption( ) throws Exception {
+        // Setup
+        final Matcher<Matched> matcher =
+                asSpecifiedBy( MatcherSpecificationWithCaseOptionOnly.class, Matched.class )
+                    .equivalentTo( new Matched( "Abc", "Def" ) );
+        // Exercise
+        final boolean match = matcher.matches( new Matched( "Abc", "def" ) );
+        // Verify
+        assertThat( match, is( true ) );
+    }
+
+    @Test
+    public void describeTextEquivalenceWithCaseOption( ) throws Exception {
+        // Setup
+        final Matcher<Matched> matcher =
+                asSpecifiedBy( MatcherSpecificationWithCaseOptionOnly.class, Matched.class )
+                    .equivalentTo( new Matched( "Abc", "Def" ) );
+        // Exercise
+        final String description = describe( matcher );
+        // Verify
+        assertThat( description, is( equalTo( "" ) ) );
+    }
 
     public static class Matched {
         public Matched( final String first, final String second ) {
@@ -24,11 +57,11 @@ public class TextMatchersTest {
     }
 
     @EquivalenceSpecificationOn( Matched.class )
-    public static interface MatcherSpecification {
-        @Text( options = { } )
+    public static interface MatcherSpecificationWithCaseOptionOnly {
+        @Text
         String getFirst( );
 
-        @Identity
+        @Text( options = IGNORE_CASE )
         String getSecond( );
     }
 
