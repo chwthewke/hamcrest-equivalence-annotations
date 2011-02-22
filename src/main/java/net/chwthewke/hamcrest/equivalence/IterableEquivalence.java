2cb51550192f80a8e36065451fabbeab2725d2c8
Thomas Dufour <chwthewke@gmail.com>
2011-02-22 23:30:26 +0100

    [hamcrest-equivalence-annotations] * removed previously deprecated IterableEquivalences factory class

--- src/main/java/net/chwthewke/hamcrest/equivalence/IterableEquivalence.java
+++ src/main/java/net/chwthewke/hamcrest/equivalence/IterableEquivalence.java
@@ -11,8 +11,23 @@ import org.hamcrest.Matcher;
 
 import com.google.common.base.Function;
 
+/**
+ * {@link IterableEquivalence} is an equivalence that acts upon Iterables of a type given an equivalence on
+ * the component type. The resulting equivalence may optionally ignore the order of elements in the sequences it
+ * receives.
+ * 
+ * @param <T>
+ *            The component type.
+ */
 public final class IterableEquivalence<T> implements Equivalence<Iterable<? extends T>> {
 
+    /**
+     * Partial evaluation.
+     * 
+     * @param expected
+     *            An iterable of the component type.
+     * @return A {@link Matcher} that matches {@link Iterable}s equivalent to <code>expected</code>.
+     */
     public Matcher<Iterable<? extends T>> equivalentTo( final Iterable<? extends T> expected ) {
         final List<Matcher<? super T>> expectedItems =
                 transform( newArrayList( expected ),
@@ -24,6 +39,15 @@ public final class IterableEquivalence<T> implements Equivalence<Iterable<? exte
         return inOrder ? contains( expectedItems ) : containsInAnyOrder( expectedItems );
     }
 
+    /**
+     * Creates an equivalence such that two {@link Iterable}s are equivalent iff there exists a 1-to-1 mapping between their respective
+     * elements such that each pair of elements in the mapping are equivalent.
+     * 
+     * @param equivalence
+     *            The equivalence on the component type.
+     * @param inOrder
+     *            When <code>true</code>, imposes the additional restriction that the above-mentioned mapping must respect iteration order.
+     */
     public IterableEquivalence( final Equivalence<T> equivalence, final boolean inOrder ) {
         this.equivalence = equivalence;
         this.inOrder = inOrder;
