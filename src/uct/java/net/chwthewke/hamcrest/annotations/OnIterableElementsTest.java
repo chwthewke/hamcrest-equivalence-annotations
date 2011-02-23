f87e2651b525a38c21cf608d518a2a27399725c5
Thomas Dufour <chwthewke@gmail.com>
2011-02-23 08:39:16 +0100

    [hamcrest-equivalence-annotations] * @OnIterableElements: initial use case test

---
+++ src/uct/java/net/chwthewke/hamcrest/annotations/OnIterableElementsTest.java
@@ -0,0 +1,57 @@
+package net.chwthewke.hamcrest.annotations;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static net.chwthewke.hamcrest.equivalence.EquivalenceClassMatchers.equates;
+import static net.chwthewke.hamcrest.equivalence.TextEquivalenceOption.IGNORE_CASE;
+import static net.chwthewke.hamcrest.matchers.Equivalences.asSpecifiedBy;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.Collection;
+
+import net.chwthewke.hamcrest.equivalence.Equivalence;
+import net.chwthewke.hamcrest.equivalence.EquivalenceClassMatchers;
+
+import org.junit.Test;
+
+public class OnIterableElementsTest {
+
+    @Test
+    public void equivalenceInOrderIgnoringCase( ) throws Exception {
+        // Setup
+        final Equivalence<Matched> equivalence = asSpecifiedBy( EqualIgnoringCaseInOrder.class, Matched.class );
+        // Exercise
+
+        // Verify
+        assertThat( equivalence,
+            equates( new Matched( "abc", "def" ), new Matched( "abc", "def" ), new Matched( "ABC", "DeF" ) ) );
+        assertThat( equivalence,
+            EquivalenceClassMatchers.separates(
+                new Matched( "abc", "def" ), new Matched( "def", "abc" ),
+                new Matched( "ABC" ), new Matched( "abc", "deg" ) ) );
+    }
+
+    public static class Matched {
+        public Matched( final String... words ) {
+            this.words = newArrayList( words );
+        }
+
+        public Collection<String> getWords( ) {
+            return words;
+        }
+
+        @Override
+        public String toString( ) {
+            return String.format( "Matched [words=%s]", words );
+        }
+
+        private final Collection<String> words;
+    }
+
+    @EquivalenceSpecificationOn( Matched.class )
+    public static interface EqualIgnoringCaseInOrder {
+        @Text( options = IGNORE_CASE )
+        @OnIterableElements( elementType = String.class )
+        Collection<String> getWords( );
+    }
+
+}
