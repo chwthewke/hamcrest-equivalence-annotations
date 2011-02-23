932583a56a7833cf027fc0d0f0484650a5849fa2
Thomas Dufour <chwthewke@gmail.com>
2011-02-23 08:49:16 +0100

    [hamcrest-equivalence-annotations] * @OnIterableElements: inOrder parameter * BasicTypeEquivalenceComputer: extra test

--- src/main/java/net/chwthewke/hamcrest/annotations/OnIterableElements.java
+++ src/main/java/net/chwthewke/hamcrest/annotations/OnIterableElements.java
@@ -8,5 +8,7 @@ import java.lang.annotation.Target;
 @Retention( RetentionPolicy.RUNTIME )
 @Target( ElementType.METHOD )
 public @interface OnIterableElements {
-    Class<?> elementType( );
+    Class<?> elementType( ) default Object.class;
+
+    boolean inOrder( ) default true;
 }
