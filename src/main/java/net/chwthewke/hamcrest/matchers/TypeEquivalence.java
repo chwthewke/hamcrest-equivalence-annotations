40770d4a95adea7c2fa1499484487815fc3b11af
Thomas Dufour <chwthewke@gmail.com>
2011-02-28 16:49:23 +0100

    [hamcrest-equivalence-annotations] * (wip) Removing the now-useless TypeEquivalence class

--- src/main/java/net/chwthewke/hamcrest/matchers/TypeEquivalence.java
+++ src/main/java/net/chwthewke/hamcrest/matchers/TypeEquivalence.java
@@ -2,22 +2,16 @@ package net.chwthewke.hamcrest.matchers;
 
 import net.chwthewke.hamcrest.equivalence.Equivalence;
 
-// TODO is this type still useful
+@Deprecated
 class TypeEquivalence<U> {
 
     public TypeEquivalence( final Equivalence<U> equivalence, final Class<U> type ) {
         this.equivalence = equivalence;
-        this.type = type;
     }
 
     public Equivalence<U> getEquivalence( ) {
         return equivalence;
     }
 
-    public Class<U> getType( ) {
-        return type;
-    }
-
     private final Equivalence<U> equivalence;
-    private final Class<U> type;
 }
